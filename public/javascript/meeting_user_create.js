8330369a748929e09f76f0641a017f92833e1a01
ITO MINORU <mito@mikilab.doshisha.ac.jp>
2018-09-22 22:54:39 +0900

    before pull

---
+++ public/javascript/meeting_user_create.js
@@ -0,0 +1,19 @@
+function meeting_user_create(idx){
+    
+    var element = document.getElementById("title");
+    element.innerHTML = idx;
+    // create person class
+    var person = document.createElement('div');
+        person.className="person"
+    // create name
+    var 
+
+    // create icon
+    var image = new Image();
+    image.src = "./img/icon";
+    image.className="img";
+    image.align="left"
+
+    //create 
+    
+};

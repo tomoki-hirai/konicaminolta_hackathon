de8e29cc1eb96fe4f997a43bed11b4a9be473024
Akihiro Kanda <akanda@mikilab.doshisha.ac.jp>
2018-09-22 16:19:28 +0900

    npm install

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.sendfile('./views/Top.html');
+  res.render('top', { title: 'Express' });
+  // res.sendfile('./viewtop.ejs');
 });
 
 module.exports = router;

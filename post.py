5e93e01b8cfd4353f2d77f4a58186a2aa6de3675
tomoki-hirai <thirai@mikilab.doshisha.c.jp>
2018-09-23 10:47:30 +0900

    image追加

--- post.py
+++ post.py
@@ -4,14 +4,14 @@ if __name__ == '__main__':
     # url = "http://ec2-13-231-238-116.ap-northeast-1.compute.amazonaws.com:3000/meeting/speaking"
     # url = "http://localhost:3000/users"
     # url = "http://ec2-13-231-238-116.ap-northeast-1.compute.amazonaws.com:3000/meeting/myturn"
-    url = "http://localhost:3000/meeting/myturn"
-    # url = "http://ec2-13-231-238-116.ap-northeast-1.compute.amazonaws.com:3000/meeting/speaking"
+    # url = "http://localhost:3000/meeting/speaking"
+    url = "http://ec2-13-231-238-116.ap-northeast-1.compute.amazonaws.com:3000/meeting/speaking"
 
     method = "POST"
     headers = {"Content-Type" : "application/json"}
 
-    # obj = {"user_id" : "warota" , "start" : "test" , "end" : "test" , "room_id" : "1" }
-    obj = {"user_id" : "5" , "user_name" : "hirai"}
+    obj = {"user_id" : "1" , "start" : "2" , "end" : "" , "room_id" : "1" }
+    # obj = {"user_id" : "5" , "user_name" : "hirai"}
     # obj = {"room_id" : "2" , "room_name" : "OSU" , "flag" : "true"}
     json_data = json.dumps(obj).encode("utf-8")
 
